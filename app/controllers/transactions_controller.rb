45b522847ffa95fb08e16b717e126f5cbfe3bad7
Franco <franco@wemake.pe>
2024-05-06 18:11:25 -0500

    Add new views and routes for transactions

---
+++ app/controllers/transactions_controller.rb
@@ -0,0 +1,14 @@
+class TransactionsController < ApplicationController
+
+	# Lista de transactions
+	def index
+	end
+
+	# Nuevo Altoquepe -> Nuevo envio
+	def new
+	end
+
+	# Cuando enviamos correctamente el yapeo
+	def success
+	end
+end
