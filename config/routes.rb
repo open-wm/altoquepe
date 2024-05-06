d76bd6aab66e7083802f9c321c1b01756a5bc49b
Franco <franco@wemake.pe>
2024-05-06 17:42:17 -0500

    Add some style to form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users # crud de users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
